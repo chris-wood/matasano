fed561914f82d9e1154f3536d6442e38c1a4c70a
Christopher Wood <christopherwood07@gmail.com>
2015-06-29 09:35:59 -0700

    byte cipher, challenge 3

---
+++ set1/byte_cipher.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import binascii
+from xor import *
+
+def main(args):
+	ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode("hex")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
